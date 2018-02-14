c5abb51b6b82d6f6b0b77ab9aad053c19fcc08dc
Karan Sehgal <d58340@solar.cat.com>
2018-02-13 19:20:24 -0800

    Corrected Warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,7 +3,6 @@ package com.example;
 /**
  * This is a class.
  */
- 
 public class Greeter {
 
   /**
@@ -12,13 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-/**
- *@param someone, name of person
- * @return greeting string
- * 
- */
 
-    public final String greet(final String someone) {
+  /**
+ * @param someone name of person
+ * @return greeting string
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
