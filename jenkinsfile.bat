5a79e7c23650530fe4c919921db03b86e937bccd
Karan Sehgal <d58340@solar.cat.com>
2018-02-17 15:11:30 -0800

    Hello

---
+++ jenkinsfile.bat
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Local Maven'
+    }
+    stages{
+        stage('Build'){
+            steps {
+                sh 'mvn clean package'
+            }
+            post {
+                success {
+                    echo 'Now Archiving...'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+    }
+}
